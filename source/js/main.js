bde1008a2d36441b2e835b649867523d81baca1d
tis <tisbootis@gmail.com>
2020-06-09 20:27:10 +0300

    add js for search and accessible menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,31 @@
+// кастомный language-select
 $('.custom-select-js').customSelect();
 $('.custom-select').length != 0 ? $('.select-lang').addClass('custom') : $('.select-lang').removeClass('custom');
 
+$('.search-toggle').click(function() {
+  $(this).toggleClass('active');
+  $('.main-nav__list').toggleClass('search-active');
+  $('.search').toggleClass('active');
+
+  if($('.search').hasClass('active')) {
+    $('.search__input').focus();
+  }
+});
+
+// доступное навигационное меню (enter и пробел)
+$('.main-nav-sub').on('keydown', function (evt) {
+  if (evt.keyCode === 13 || evt.keyCode === 32) {
+    evt.preventDefault();
+    let currentSublist = $(this).closest('.main-nav__item');
+
+    $('.main-nav__item').not(currentSublist).removeClass('selected-on-tab');
+    currentSublist.toggleClass('selected-on-tab');
+
+    // клик мышкой вне выпадающего меню
+    $(document).on('mouseup', function (evt) {
+      if (!currentSublist.is(evt.target) && currentSublist.has(evt.target).length === 0) {
+        currentSublist.removeClass('selected-on-tab');
+      }
+    });
+  }
+});
