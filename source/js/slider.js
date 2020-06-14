b8471bdbe04e88b5e23e86aa52b4496bbc6d424d
tis <tisbootis@gmail.com>
2020-06-14 18:46:46 +0300

    add reviews-swiper block

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,19 +2,39 @@ let rehabSwiper = new Swiper('#rehab-swiper', {
   slidesPerView: '2',
   spaceBetween: 25,
   touchRatio: 1,
-  // speed: 1000,
 
   navigation: {
     nextEl: '.rehab-swiper__button-next',
     prevEl: '.rehab-swiper__button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.rehab-swiper__swiper-pagination',
     dynamicBullets: true,
   },
   breakpoints: {
     756: {
       slidesPerView: '4',
+      spaceBetween: 30,
+    }
+  }
+});
+
+let reviewsSwiper = new Swiper('#reviews-swiper', {
+  slidesPerView: '2',
+  spaceBetween: 25,
+  touchRatio: 1,
+
+  navigation: {
+    nextEl: '.reviews-swiper__button-next',
+    prevEl: '.reviews-swiper__button-prev',
+  },
+  pagination: {
+    el: '.reviews-swiper__swiper-pagination',
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    756: {
+      slidesPerView: '2',
       spaceBetween: 30,
     }
   }
