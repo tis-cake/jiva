ba50869d1abe4724b3d99041a14fae34ac0242e5
tis <tisbootis@gmail.com>
2020-06-15 05:07:42 +0300

    add footer

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,43 +1,45 @@
-let rehabSwiper = new Swiper('#rehab-swiper', {
-  slidesPerView: '2',
-  spaceBetween: 25,
-  touchRatio: 1,
+$(document).ready(function () {
+  let rehabSwiper = new Swiper('#rehab-swiper', {
+    slidesPerView: '2',
+    spaceBetween: 25,
+    touchRatio: 1,
 
-  navigation: {
-    nextEl: '.rehab-swiper__button-next',
-    prevEl: '.rehab-swiper__button-prev',
-  },
-  pagination: {
-    el: '.rehab-swiper__swiper-pagination',
-    dynamicBullets: true,
-  },
-  breakpoints: {
-    756: {
-      slidesPerView: '4',
-      spaceBetween: 30,
+    navigation: {
+      nextEl: '.rehab-swiper__button-next',
+      prevEl: '.rehab-swiper__button-prev',
+    },
+    pagination: {
+      el: '.rehab-swiper__swiper-pagination',
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      756: {
+        slidesPerView: '4',
+        spaceBetween: 30,
+      }
     }
-  }
-});
+  });
 
-let reviewsSwiper = new Swiper('#reviews-swiper', {
-  slidesPerView: '2',
-  spaceBetween: 25,
-  touchRatio: 1,
+  let reviewsSwiper = new Swiper('#reviews-swiper', {
+    slidesPerView: '2',
+    spaceBetween: 25,
+    touchRatio: 1,
 
-  navigation: {
-    nextEl: '.reviews-swiper__button-next',
-    prevEl: '.reviews-swiper__button-prev',
-  },
-  pagination: {
-    el: '.reviews-swiper__swiper-pagination',
-    dynamicBullets: true,
-  },
-  breakpoints: {
-    756: {
-      slidesPerView: '2',
-      spaceBetween: 30,
+    navigation: {
+      nextEl: '.reviews-swiper__button-next',
+      prevEl: '.reviews-swiper__button-prev',
+    },
+    pagination: {
+      el: '.reviews-swiper__swiper-pagination',
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      756: {
+        slidesPerView: '2',
+        spaceBetween: 30,
+      }
     }
-  }
+  });
 });
 
 // reviewsSwiper.on('slideChange', function () {
