9efe2caa94e69326e1fe2861d7b9c7b6c8dd04e0
tis <tisbootis@gmail.com>
2020-06-21 16:27:47 +0300

    add map

--- source/js/map.js
+++ source/js/map.js
@@ -2,16 +2,33 @@ ymaps.ready(init);
 
 function init() {
   var map = new ymaps.Map('map', {
-    center: [55.715270, 37.355007],
+    center: [55.719270, 37.625007],
     zoom: 11,
     controls: [],
     behaviors: ['drag', 'dblClickZoom']
   });
 
-  // дефолтный маркер
-  var myPlacemark = new ymaps.Placemark([55.715270, 37.355007], {}, {
-    preset: 'islands#redIcon'
-  });
+  var iconParam = {
+    iconLayout: 'default#image',
+    iconImageHref: '/img/marker-maps.png',
+    iconImageSize: [40, 45]
+  }
+
+
+  myPlacemark = new ymaps.Placemark([55.715270, 37.355007], {
+      balloonContent: 'Рехаб 1'
+  }, iconParam );
+
+  myPlacemark2 = new ymaps.Placemark([55.719270, 37.625007], {
+      balloonContent: 'Рехаб 2'
+  }, iconParam );
+
+  myPlacemark3 = new ymaps.Placemark([55.759270, 37.625007], {
+      balloonContent: 'Рехаб 3'
+  }, iconParam );
+
 
   map.geoObjects.add(myPlacemark);
+  map.geoObjects.add(myPlacemark2);
+  map.geoObjects.add(myPlacemark3);
 }
